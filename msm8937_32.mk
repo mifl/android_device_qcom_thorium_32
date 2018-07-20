84e7fbac589e17173e7d00e2dc30bd007e363454
Mohamed Sunfeer <msunfeer@codeaurora.org>
2018-07-20 12:19:31 +0530

    msm8937_32: Enable hardware keymaster v4.0
    Enable keymaster v4.0 and also adds keymaster
    xml for hidl interfaces.
    
    Change-Id: I05406e5529a0e661ef448eb28533f136f0e3e633

--- msm8937_32.mk
+++ msm8937_32.mk
@@ -295,6 +295,17 @@ PRODUCT_PACKAGES += android.hardware.gatekeeper@1.0-impl \
                     android.hardware.keymaster@3.0-service
 endif
 
+#Enable KEYMASTER 4.0 for Android P not for OTA's
+ifeq ($(strip $(TARGET_KERNEL_VERSION)), 4.9)
+    ENABLE_KM_4_0 := true
+endif
+
+ifeq ($(ENABLE_KM_4_0), true)
+    DEVICE_MANIFEST_FILE += device/qcom/msm8937_32/keymaster.xml
+else
+    DEVICE_MANIFEST_FILE += device/qcom/msm8937_32/keymaster_ota.xml
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so
 
 ifeq ($(TARGET_HAS_LOW_RAM), true)
