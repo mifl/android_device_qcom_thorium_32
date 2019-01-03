817e106def17da0950e064e1046cffb1f7fd59b8
Deepthi Gunturi <dgunturi@codeaurora.org>
2019-01-03 10:31:59 +0530

    Moving vendor_framework_compatibility_matrix.xml to core-utils
    Change-Id: Ifede83dce886f48d926892814484f5824fb8f982

--- msm8937_32.mk
+++ msm8937_32.mk
@@ -100,7 +100,7 @@ DEVICE_MANIFEST_FILE := device/qcom/msm8937_32/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/msm8937_32/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    device/qcom/common/vendor_framework_compatibility_matrix.xml
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 ifneq ($(strip $(QCPATH)),)
     PRODUCT_BOOT_JARS += WfdCommon
     PRODUCT_BOOT_JARS += oem-services
