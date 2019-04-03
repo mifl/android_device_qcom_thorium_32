ae0a17f0910bb399922845846ad95405385d5da9
Karthik Gopalan <gkarth@codeaurora.org>
2019-04-03 11:28:28 +0530

    msm8937_32.mk: Enable perf properties
    Set ro.vendor.qti.config.swap property to true
    
    CRs-Fixed: 2429291
    
    Change-Id: I4528fbcc9784e790f6724d49a6f3e9426c69f6ad

--- msm8937_32.mk
+++ msm8937_32.mk
@@ -78,6 +78,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, device/qcom/common/common.mk)
 
+#Property to enable Swap
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.qti.config.swap=1
+
 PRODUCT_NAME := msm8937_32
 PRODUCT_DEVICE := msm8937_32
 
